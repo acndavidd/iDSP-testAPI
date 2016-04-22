d4cdc7ae1a095b4aee73e246531f044c92db0b50
Acnhendriks <sutrisnahendrik@gmail.com>
2016-04-22 14:35:24 +0700

    Baseline iDSP

---
+++ src/app/app/login/component/login.component.ts
@@ -0,0 +1,13 @@
+import {Component} from 'angular2/core';
+
+
+@Component({
+    selector: 'login',
+    templateUrl: './app/login/components/login.html'
+})
+export class LoginComponent {
+    
+	constructor () {
+		
+	}
+}
