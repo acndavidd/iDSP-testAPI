87864ecebb93c6e80e78d4d041ffb586d580f80e
Ediyono <edi.yono@accenture.com>
2016-05-02 15:02:21 +0700

    Add ORMSErvice to api

--- src/api/controllers/login.controller.js
+++ src/api/controllers/login.controller.js
@@ -1,17 +1,44 @@
 'use strict';
 const token_service_1 = require('../services/token.service');
+const orm_service_1 = require('../services/orm.service');
 class LoginController {
     constructor() {
     }
     doLogin(req, res) {
         let tokenSvc = new token_service_1.TokenService();
-        var result = {};
-        result.token = tokenSvc.generateToken();
+        //var result = {};
+        var tokenobj = {
+            user: {
+                name: req.body.username,
+                password: req.body.password
+            },
+            success: 1
+        };
+        /*tokenobj.user = {};
+        tokenobj.user.name = req.body.username;
+        tokenobj.user.password = req.body.password;
+        result.success = 1;*/
+        var result = {
+            token: tokenSvc.generateToken(tokenobj)
+        };
+        //result.token = tokenSvc.generateToken(tokenobj);
         res.cookie('accessToken', result.token, { httpOnly: true });
         res.json(result);
     }
-    checkToken(req, res) {
-        res.json('calling checkToken ' + req.body.token);
+    verifyToken(token) {
+        let tokenSvc = new token_service_1.TokenService();
+        try {
+            var verify = tokenSvc.verifyToken(token);
+            return verify;
+        }
+        catch (err) {
+            throw err;
+        }
+    }
+    sp(req, res) {
+        let ormSvc = new orm_service_1.ORMService();
+        var user = 'djoko';
+        ormSvc.executeFunction('anjay', JSON.stringify(user));
     }
 }
 exports.LoginController = LoginController;
