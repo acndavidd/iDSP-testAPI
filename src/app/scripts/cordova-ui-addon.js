d99bc87fba389abcc63dd68e68293257221e564b
Christoper Hans <christoper.hans@accenture.com>
2016-05-12 16:12:39 +0700

    add cordova configuration

--- src/app/scripts/cordova-ui-addon.js
+++ src/app/scripts/cordova-ui-addon.js
@@ -1,6 +1,15 @@
-document.addEventListener("backbutton", onBackKeyDown, false);
+function onLoad() {
+	document.addEventListener("backbutton", onBackKeyDown, false);
+    console.log("loading");
+}
+    document.addEventListener("deviceready", onDeviceReady, false);
+
+function onDeviceReady() {
+	console.log("this device is :" + device.cordova);
+}
 
 function onBackKeyDown() {
+	window.location('/');
 	alert('test');
     // Handle the back button
 }
