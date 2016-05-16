fa800259185e1dffcfe7d606557b3e1e090d45ce
Irvinder Irianto <irvinder.irianto@gmail.com>
2016-05-16 16:48:51 +0700

    Update Hamburger UI design

--- src/app/app/shared/services/layout.service.ts
+++ src/app/app/shared/services/layout.service.ts
@@ -1,4 +1,4 @@
- import {Injectable} from 'angular2/core';
+import {Injectable} from 'angular2/core';
 import {PageNavigationService} from './page-navigation.service';
 import {MatchMediaService} from './match-media.service';
 
