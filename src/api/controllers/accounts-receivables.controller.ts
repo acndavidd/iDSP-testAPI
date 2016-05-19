7f2deca6410e2e4e3b74bcb730c2eaa4a18339f9
acnleonf <leon.febri@MW7KLZJ3KC0T4Q.dir.svc.accenture.com>
2016-05-19 15:39:52 +0700

    fix account receivables controller, change model to sp

--- src/api/controllers/accounts-receivables.controller.ts
+++ src/api/controllers/accounts-receivables.controller.ts
@@ -9,50 +9,67 @@ export class AccController{
 	constructor(){ 		
 	}
 
-	getAccountsReceivables(pRequest,pResponse){
+	async getAccountsReceivables(pRequest, pResponse){
 		try{
 			console.log('in getAccountsReceivables API');
 			let vOrmSvc = new ORMService();
-			let vAccModel = vOrmSvc.getModel('trx_sales_order');
+			// let vAccModel = vOrmSvc.getModel('trx_sales_order');
+			var vDspId = pRequest.body.vDspId;
+			var vDate = parseInt(pRequest.body.vDate);
 
-			let vResult = [];
-			var vPromises = [];
-			var vDate = new Date().getDay();
-			var vDspId = 'DSP00001';
+			let vParams = {
+				dsp_id : vDspId,
+				route_day : vDate
+			};
 
-			vAccModel.findAll({
-				attributes : ['order_id'],
-				include : [{
-					model : vOrmSvc.getModel('trx_account_receivable'),
-					as : 'AccountReceivable',
-					required : true,
-					attributes : ['amount', 'dsp_id'],
-					where : {dsp_id : vDspId}, 
-					include :[{
-						model : vOrmSvc.getModel('mst_retailer'),
-						as : 'Retailer',
-						required : true,
-						attributes : ['retailer_name', 'retailer_min'],
-						include : [{
-							model : vOrmSvc.getModel('mst_route'),
-							as : 'Route',
-							attributes : ['route_id'],
-							include : [{
-								model : vOrmSvc.getModel('mst_route_day'),
-								as : 'RouteDay',
-								required : true,
-								where : { route_day : vDate},
-								attributes : ['sequence']
-							}]
-						}]
-					}]
-				}]
-			}).then(function(ret){
-				pResponse.json(ret);
-			});
+			var vResult = await vOrmSvc.sp( 'get_account_receivables', vParams );
+			var vResponse = {
+				"status" : "Success",
+				"errorMessage" : "",
+				"result" : vResult
+			};
+			console.log( "get_account_receivables response : " + JSON.stringify( vResponse ) );
+			pResponse.json(vResponse);
+
+			// vAccModel.findAll({
+			// 	attributes : ['order_id'],
+			// 	include : [{
+			// 		model : vOrmSvc.getModel('trx_account_receivable'),
+			// 		as : 'AccountReceivable',
+			// 		required : true,
+			// 		attributes : ['amount', 'dsp_id'],
+			// 		where : {dsp_id : vDspId}, 
+			// 		include :[{
+			// 			model : vOrmSvc.getModel('mst_retailer'),
+			// 			as : 'Retailer',
+			// 			required : true,
+			// 			attributes : ['retailer_name', 'retailer_min'],
+			// 			include : [{
+			// 				model : vOrmSvc.getModel('mst_route'),
+			// 				as : 'Route',
+			// 				attributes : ['route_id'],
+			// 				include : [{
+			// 					model : vOrmSvc.getModel('mst_route_day'),
+			// 					as : 'RouteDay',
+			// 					required : true,
+			// 					where : { route_day : vDate},
+			// 					attributes : ['sequence']
+			// 				}]
+			// 			}]
+			// 		}]
+			// 	}]
+			// }).then(function(ret){
+			// 	pResponse.json(ret);
+			// });
 
 		}catch(pErr){
-			console.log('Error at AccController: '+pErr);
+			var vError = {
+					"status" : "Error",
+					"errorMessage" : pErr,
+					"result" : null
+				};
+			console.log( "Error in get_account_receivables : " + vError);
+			pResponse.json(vError);
 		}
 	}
 }
