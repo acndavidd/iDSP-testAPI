1ef1d4c67188a65345273320f50d629a3cd64e04
acnleonf <leon.febri@MW7KLZJ3KC0T4Q.dir.svc.accenture.com>
2016-05-19 17:57:19 +0700

    add source cell in account receivables

--- src/app/app/my-transaction/components/accounts-receivables.component.ts
+++ src/app/app/my-transaction/components/accounts-receivables.component.ts
@@ -10,7 +10,8 @@ import {NgFor, NgModel} from 'angular2/common';
 
 @Component({
     selector: 'accounts-receivables',
-    templateUrl: './app/my-transaction/components/hc-accounts-receivables.component.html',
+    //templateUrl: './app/my-transaction/components/hc-accounts-receivables.component.html',
+    templateUrl: './app/my-transaction/components/accounts-receivables.component.html',
     directives: [
         NgFor, NgModel, ROUTER_DIRECTIVES
     ],
@@ -40,10 +41,10 @@ export class AccountsReceivablesComponent {
         var vDate = new Date().getDay();
         console.log( 'vDate: ' + vDate );
 
-        this._accountsReceivablesService.getAllRetailer(vDspId,vDate).subscribe(
+        this._accountsReceivablesService.getAllRetailer( vDspId, vDate ).subscribe(
             response => {
                 this.setAllRetailerList(response.json().result);
-                console.log( 'response success' );
+                console.log( 'response success dapet source ' + response.json().result[0].source);
                 console.log(JSON.stringify(response.json()));
                 console.log(response.json().result.length);
                 this.setTotalReceivable(parseInt(response.json().result[0].total_amount).toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,'));
