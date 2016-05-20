d557a71bebafdc0fc2fad0032ca09a228a6ba4df
acnleonf <leon.febri@MW7KLZJ3KC0T4Q.dir.svc.accenture.com>
2016-05-20 10:29:00 +0700

    fix tslint and css in account receivables

--- src/app/app/my-transaction/components/accounts-receivables.component.ts
+++ src/app/app/my-transaction/components/accounts-receivables.component.ts
@@ -10,7 +10,7 @@ import {NgFor, NgModel} from 'angular2/common';
 
 @Component({
     selector: 'accounts-receivables',
-    //templateUrl: './app/my-transaction/components/hc-accounts-receivables.component.html',
+    // templateUrl: './app/my-transaction/components/hc-accounts-receivables.component.html',
     templateUrl: './app/my-transaction/components/accounts-receivables.component.html',
     directives: [
         NgFor, NgModel, ROUTER_DIRECTIVES
@@ -67,7 +67,7 @@ export class AccountsReceivablesComponent {
     }
 
     getTotalReceivable() {
-        if (this.vSum == null) {
+        if ( this.vSum === null ) {
             this.setTotalReceivable(0);
         }
         return this.vSum;
