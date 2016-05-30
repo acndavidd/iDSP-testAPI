abb10ef9d37386e83cf06bf41147dc1229cf6bd5
Bianda Nathania <bianda.nathania@accenture.com>
2016-05-30 18:34:29 +0700

    updated mpin backend

--- src/app/app/login/components/mpin.component.ts
+++ src/app/app/login/components/mpin.component.ts
@@ -47,8 +47,8 @@ export class MpinComponent {
         pEvent.preventDefault();
         // this._authenticationService.submitMPIN(this.vMPIN1 + this.vMPIN2 + this.vMPIN3 + this.vMPIN4 + this.vMPIN5);
         // For By Pass Directly without API
-        this._pageNavigationService.navigate('Home', null, null);
-        // this._authenticationService.submitMPIN(this.vMPIN1 + this.vMPIN2 + this.vMPIN3 + this.vMPIN4 + this.vMPIN5);
+        // this._pageNavigationService.navigate('Home', null, null);
+        this._authenticationService.submitMPIN(this.vMPIN1 + this.vMPIN2 + this.vMPIN3 + this.vMPIN4 + this.vMPIN5);
     }
 
     ResendMpinModalComponent() {
