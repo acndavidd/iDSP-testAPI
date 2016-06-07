7ae96e6c1911781af489192f98ca00e56b76d8af
Acnagnes <agnesganisa@gmail.com>
2016-06-07 18:06:34 +0700

    Rapi-rapi css (left menu header, change to thicker icon, change peso icon, add rounded for input & select)

--- src/app/app/shared/config/service.config.ts
+++ src/app/app/shared/config/service.config.ts
@@ -1,4 +1,4 @@
 export module ServiceConfiguration {
-	export const BaseURL = 'http://127.0.0.1:8080/service';
-	export const Timeout = 15000;
+    export const BaseURL = 'http://127.0.0.1:8080/service';
+    export const Timeout = 15000;
 }
